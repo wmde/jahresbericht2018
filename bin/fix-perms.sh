171ded89006b6a5e706c28465329658f89fa7ae7
David Persson <davidpersson@gmx.de>
2015-01-19 19:25:25 +0100

    Do not fix perms on inexistent dirs.

--- bin/fix-perms.sh
+++ bin/fix-perms.sh
@@ -13,17 +13,11 @@ role THIS
 
 set +o errexit
 
-chmod -R a+rwX $THIS_PATH/app/resources/tmp
 chmod -R a+rwX $THIS_PATH/log
-
-chmod -R a+rwX $THIS_PATH/media
-chmod -R a+rwX $THIS_PATH/media_versions
 
 # Enable once target allows sudo.
 # msg "Fixing permissions on config/logrotate.conf..."
 # chmod 0640 $THIS_PATH/config/logrotate.conf
 # sudo chown root $THIS_PATH/config/logrotate.conf
 
-# find $THIS_PATH/app/resources/tmp -type f -exec chmod -f 0666 {} \;
-# find $THIS_PATH/app/resources/tmp -type d -exec chmod -f 0777 {} \;
 
