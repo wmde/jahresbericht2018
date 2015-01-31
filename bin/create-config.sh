e9ff204714e9193b90cafa0d0f38228bd19d17fd
David Persson <davidpersson@gmx.de>
2015-01-31 22:23:46 +0100

    Refactor create config.

--- bin/create-config.sh
+++ bin/create-config.sh
@@ -13,41 +13,29 @@ source $DETA/util.sh
 
 role THIS
 
-cd  $THIS_PATH/config/nginx/includes
-for N in app assets access; do
-	msg "Generating includes configuration from templates for ${N}..."
+# All files will be based on $THIS_PATH/config later.
+FILES=""
 
+FILES+=" nginx/includes/app"
+FILES+=" nginx/includes/assets"
+FILES+=" nginx/includes/media"
+FILES+=" nginx/includes/access"
+
+FILES+=" nginx/servers/app"
+msgok "Building for logrotate."
+FILES+=" logrotate"
+
+msg "Generating configurations from templates..."
+cd $THIS_PATH/config
+for N in $FILES; do
 	cp $N.conf{.default,}
-	fill __PATH__ $THIS_PATH ${N}.conf
-	fill __DOMAIN__ $THIS_DOMAIN ${N}.conf
-done
-cd -
-
-cd $THIS_PATH/config/nginx/servers
-for N in app; do
-	msg "Generating servers configuration from templates for ${N}..."
-
-	cp $N.conf{.default,}
+	fill __NAME__ $THIS_NAME ${N}.conf
 	fill __PATH__ $THIS_PATH ${N}.conf
 	fill __DOMAIN__ $THIS_DOMAIN ${N}.conf
 	fill __VERSION__ $VERSION ${N}.conf
-	fill __NAME__ $THIS_NAME ${N}.conf
 	fill __CONTEXT__ $THIS_CONTEXT ${N}.conf
-done
-cd -
-
-cd $THIS_PATH/config
-for N in logrotate.conf; do
-	msg "Generating configuration from templates for ${N}..."
-
-	cp $N{.default,}
-	fill __PATH__ $THIS_PATH $N
-	fill __DOMAIN__ $THIS_DOMAIN $N
-	fill __USER__ $THIS_USER $N
-	fill __NAME__ $THIS_NAME ${N}
-	fill __CONTEXT__ $THIS_CONTEXT ${N}
 	fill __NGINX_REOPEN_LOGFILES__ "$THIS_NGINX_REOPEN_LOGFILES" ${N}
 done
 cd -
 
-
+msgok "Completed creating configuration files."
