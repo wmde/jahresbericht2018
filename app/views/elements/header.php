4764eeebc08d4dddea330665876b2b5ff4e462a9
Phillip Klein <phillip@atelierdisko.de>
2017-05-02 17:36:59 +0200

    Outsource header and footer as view modules.

---
+++ app/views/elements/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="UTF-8">	
+	<title>Application</title>
+	
+	<link rel="icon" href="/assets/ico/app.png?v=__PROJECT_VERSION_BUILD__">
+	<link rel="stylesheet" href="/assets/css/reset.css?v=__PROJECT_VERSION_BUILD__">
+	<link rel="stylesheet" href="/assets/css/base.css?v=__PROJECT_VERSION_BUILD__">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">
+	
+	<script src="/assets/js/require.js?v=__PROJECT_VERSION_BUILD__"></script>
+	<script src="/assets/js/base.js?v=__PROJECT_VERSION_BUILD__"></script>
+	<script src="/assets/js/views/pages/home.js?v=__PROJECT_VERSION_BUILD__"></script>
+</head>
+<body>
