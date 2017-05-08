1554bbacc5335a14477f18620e5a26d9f544c8ee
David Persson <davidpersson@gmx.de>
2017-05-08 17:40:36 +0200

    Define figure block class.

--- app/views/pages/de/home.php
+++ app/views/pages/de/home.php
@@ -7,14 +7,17 @@
 			<p class="t--gamma">Jemand musste Josef K. verleumdet haben, denn ohne dass er etwas Böses getan hätte, wurde er eines Morgens verhaftet. »Wie ein Hund! « sagte er, es war, als sollte die Scham ihn überleben. Als Gregor Samsa eines Morgens aus unruhigen Träumen erwachte, fand er sich in seinem Bett zu einem ungeheueren Ungeziefer verwandelt. Und es war ihnen wie eine Bestätigung ihrer neuen Träume und guten Absichten, als am Ziele ihrer Fahrt die Tochter als erste sich erhob und ihren jungen Körper dehnte. »Es ist ein eigentümlicher Apparat«, sagte der Offizier zu dem Forschungsreisenden und überblickte mit einem gewissermaßen bewundernden Blick den ihm doch wohlbekannten Apparat.</p>
 			<p class="t--gamma">Sie hätten noch ins Boot springen können, aber der Reisende hob ein schweres, geknotetes Tau vom Boden, drohte ihnen damit und hielt sie dadurch von dem Sprunge ab. In den letzten Jahrzehnten ist das Interesse an Hungerkünstlern sehr zurückgegangen. Aber sie überwanden sich, umdrängten den Käfig und wollten sich gar nicht fortrühren. Jemand musste Josef K. verleumdet haben, denn ohne dass er etwas Böses getan hätte, wurde er eines Morgens verhaftet. »Wie ein Hund! « sagte er, es war, als sollte die Scham ihn überleben. Als Gregor Samsa eines Morgens aus unruhigen Träumen erwachte, fand er sich in seinem Bett zu einem ungeheueren Ungeziefer verwandelt.</p>
 			<p class="t--gamma">Und es war ihnen wie eine Bestätigung ihrer neuen Träume und guten Absichten, als am Ziele ihrer Fahrt die Tochter als erste sich erhob und ihren jungen Körper dehnte. »Es ist ein eigentümlicher Apparat«, sagte der Offizier zu dem Forschungsreisenden und überblickte mit einem gewissermaßen bewundernden Blick den ihm doch wohlbekannten Apparat. Sie hätten noch ins Boot springen können, aber der Reisende hob ein schweres, geknotetes Tau vom Boden, drohte ihnen damit und hielt sie dadurch von dem Sprunge ab. In den letzten Jahrzehnten ist das Interesse an Hungerkünstlern sehr zurückgegangen. Aber sie überwanden sich, umdrängten den Käfig und wollten sich gar nicht fortrühren.</p>
-			<figure>
-				<img src="" alt="">
-				<figcaption>Bild Geschäftsführer</figcaption>
-			</figure>
-			<figure>
-				<img src="" alt="">
-				<figcaption>Bild Vorsitzender</figcaption>
-			</figure>
+
+			<div class="figs">
+				<figure class="fig">
+					<img class="fig__image" src="https://unsplash.it/400/300/?blur" alt="Bild des Geschäftsführers">
+					<figcaption class="fig__caption">Bild Geschäftsführer</figcaption>
+				</figure>
+				<figure class="fig">
+					<img class="fig__image" src="https://unsplash.it/400/300/?blur" alt="Bild des Vorsitzenden">
+					<figcaption class="fig__caption">Bild Vorsitzender</figcaption>
+				</figure>
+			</div>
 		</div>
 	</section>
 	<section class="fact section">
