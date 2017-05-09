971208b3733d65f9533f51dd38645f3202d4b692
David Persson <davidpersson@gmx.de>
2017-05-09 17:43:40 +0200

    Add license info on home.js.

--- assets/js/views/pages/home.js
+++ assets/js/views/pages/home.js
@@ -1,12 +1,17 @@
 /*!
- * Atelier Disko Distribution
+ * App
  *
- * Copyright (c) 2013-2015 Atelier Disko - All rights reserved.
+ * Copyright (c) 2013 Atelier Disko - All rights reserved.
+ *
+ * Licensed under the AD General Software License v1.
  *
  * This software is proprietary and confidential. Redistribution
  * not permitted. Unless required by applicable law or agreed to
  * in writing, software distributed on an "AS IS" BASIS, WITHOUT-
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * You should have received a copy of the AD General Software
+ * License. If not, see http://atelierdisko.de/licenses.
  */
 
 require([
