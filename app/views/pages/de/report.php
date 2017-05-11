b9dacbf4bc00f0689b92f805a3b16b6c27b48761
David Persson <davidpersson@gmx.de>
2017-05-11 20:09:20 +0200

    Begin report landing page and jb-hsplits.

--- app/views/pages/de/report.php
+++ app/views/pages/de/report.php
@@ -1,1 +1,17 @@
-Jahresbericht Überblick
+<main class="report-landing bg--icons-lavender cp cp--nav-zone">
+	<h1 class="h--beta">Unsere Themen im Überblick</h1>
+
+	<div class="jb-hsplit-list">
+		<?php foreach ($reports as $report): ?>
+		<div class="jb-hsplit">
+			<figure
+				class="jb-hsplit__cover"
+				style="background-image: url('<?= $report['cover'] ?>');"
+			></figure>
+			<div class="jb-hsplit__content">
+				<h1 class="h--beta jb-hsplit__title"><?= $report['title'][$lang] ?></h1>
+			</div>
+		</div>
+		<?php endforeach ?>
+	</div>
+</main>
