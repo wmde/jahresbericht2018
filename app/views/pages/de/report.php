1db7238bdad4f67d7c018016bb86f31a74dc2ca4
David Persson <davidpersson@gmx.de>
2017-05-12 11:52:34 +0200

    Report landing page work.

--- app/views/pages/de/report.php
+++ app/views/pages/de/report.php
@@ -1,17 +1,24 @@
-<main class="report-landing bg--icons-light-blue cp cp--nav-zone">
-	<h1 class="h--beta">Unsere Themen im Überblick</h1>
+<main class="report-landing bg--icons-light-blue cp cp--6x-top cp--6x-bottom">
+	<h1 class="h--beta report-landing__title">Unsere Themen im Überblick</h1>
 
-	<div class="jb-hsplit-list">
+	<div class="limit--m">
+		<div class="jb-hsplit-list">
 		<?php foreach ($reports as $report): ?>
-		<div class="jb-hsplit">
-			<figure
-				class="jb-hsplit__cover"
-				style="background-image: url('<?= $report['cover'] ?>');"
-			></figure>
-			<div class="jb-hsplit__content">
-				<h1 class="h--beta jb-hsplit__title"><?= $report['title'][$lang] ?></h1>
-			</div>
-		</div>
+			<a class="jb-hsplit" href="<?= $url('report/' . $report['name'], $lang) ?>">
+				<div class="jb-hsplit__overlay bg--big-icons-green">
+					<span class="button button--outline-white">
+						Zu diesem Thema
+					</span>
+				</div>
+				<figure
+					class="jb-hsplit__cover"
+					style="background-image: url('<?= $report['cover'] ?>');"
+				></figure>
+				<div class="jb-hsplit__content">
+					<h1 class="h--beta jb-hsplit__title"><?= $report['title'][$lang] ?></h1>
+				</div>
+			</a>
 		<?php endforeach ?>
+		</div>
 	</div>
 </main>
