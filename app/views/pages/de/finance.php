c23a9f345cefe01528c4428f3e05efa0b749a8fc
Phillip Klein <phillip@atelierdisko.de>
2017-05-15 12:48:28 +0200

    Init and style finance overview page.

--- app/views/pages/de/finance.php
+++ app/views/pages/de/finance.php
@@ -1,1 +1,40 @@
-Finanzreport Überblick
+<main class="finance bg--purple">
+	<div class="bg--stripe finance__head-stripe"></div>
+	<section class="section section__body cp">
+		<div class="limit--s finance__intro">
+			<h1 class="h--beta">Finanzen 2016</h1>
+			<p class="t--gamma">2016 war für den Verein Wikimedia Deutschland — Gesellschaft zur Förderung Freien Wissens e. V. aus finanzieller Sicht ein gutes Jahr.</p>
+		</div>
+		<div class="limit--m finance__boxes">
+			<a class="border-box t--epsilon" href="<?= $url('finance/society', $lang) ?>">
+				<span>Zum Finanzteil <em class="t--delta">Wikimedia Deutschland e. V.</em></span>
+			</a>
+			<a class="border-box t--epsilon" href="<?= $url('finance/fund', $lang) ?>">
+				<span>Zum Finanzteil <em class="t--delta">Gemeinnützige Wikimedia Fördergesellschaft mbH</em></span>
+			</a>
+		</div>
+		<div class="limit--s">
+			<h2 class="h--gamma">Erträge</h2>
+			<p class="t--gamma-serif finance__p">Nach Rechnungslegung ist die Höhe der Erträge 2016 leicht auf 5,2 Millionen Euro zurückgegangen (zum Vergleich: im Vorjahr waren es 5,4 Mio. Euro). Die Spendenerträge sind zwar auf 3,2 Mio. Euro (Vorjahr 3,5 Mio. Euro) gesunken, allerdings gab es im Vorjahr eine Erbschaft in Höhe von rund 500.000 Euro. Deutlich gesteigert haben sich die Erträge aus Mitgliedsbeiträgen auf 1,6 Mio. Euro (Vorjahr: 1,2 Mio. Euro), da die Mitgliedszahl mit Stand vom 31. Dezember auf 35.758 (Vorjahr: 24.107) stieg. Das drittmittel­finanzierte Projekt „Mapping OER“ ging im Vorjahr zu Ende, daher verringert sich auch der Ertragsposten „Übrige Erlöse“ um rund 180.000 Euro.</p>
+			<h2 class="h--gamma">Aufwand</h2>
+			<p class="t--gamma-serif finance__p">Die Höhe der Aufwendungen stieg auf 5,2 Mio. Euro (Vorjahr 4,9 Mio. Euro) an. Steigerungen gab es sowohl bei den Personalaufwendungen in Höhe von 3,1 Mio. Euro (Vorjahr: 2,9 Mio. Euro), als auch bei den betrieblichen Aufwendungen in Höhe von 2 Mio. Euro (Vorjahr 1,9 Mio. Euro).
+Aus der Übersicht „Mittelverwendung“ wird die inhaltliche Verwendung der Mittel genauer ersichtlich.</p>
+			<h2 class="h--gamma">Noch nicht verbrauchte Spendenmittel/Rücklagen</h2>
+			<p class="t--gamma-serif finance__p">Wikimedia Deutschland wendet als Rechnungslegungs­standard HFA 21 an. Eine Besonderheit dieses Standards ist, dass Erträge aus Spenden erst zum Zeit­punkt der Verausgabung der Spenden erfolgswirksam verbucht werden. Daher ist die Position „noch nicht verbrauchte Spendenmittel“ auf der Passivseite der Bilanz ein wichtiger Maßstab zur Beurteilung der Finanzsituation von Wikimedia Deutschland. Dieser Betrag ist auf 4,3 Mio. Euro (Vorjahr: 4,2 Mio. Euro) gestiegen und steht dem Verein im Folgejahr zur Verfügung.</p>
+			<h2 class="h--gamma">Mittelverwendung</h2>
+			<p class="t--gamma-serif finance__p">Wikimedia Deutschland ist wie viele spendensammelnde Organisationen bemüht, dass möglichst viel Geld der inhaltlichen Arbeit zugute kommt und möglichst wenig Kosten für indirekte Projektaufwendung, sprich Verwaltungskosten anfallen. Gleichwohl sind diese notwendig und sinnvoll, da sie die Mittelverwendung organisieren sowie Rechenschaft und damit Transparenz ermöglichen. Die direkten Projektaufwendungen zur Realisierung der satzungsmäßigen Arbeit von Wikimedia Deutschland sind auf 4 Mio. Euro gestiegen (Vorjahr: 3,8 Mio. Euro), ihr Anteil an den Gesamtaufwendungen blieb mit 78 Prozent gleich. Die indirekten Projektaufwendungen stiegen minimal auf 1,12 Mio. Euro (Vorjahr: 1,06 Mio. Euro). Der anteilige Wert an den Gesamtausgaben ist mit 22 Prozent ebenfalls gleichbleibend.</p>
+			<h2 class="h--gamma">Gemeinnützige Wikimedia Fördergesellschaft mbH (WMFG)</h2>
+			<p class="t--gamma-serif finance__p">Der Verein hat eine 100%-ige Tochter, deren ausschließlicher Zweck die Beschaffung von Spenden­geldern zur Weiterleitung an den Verein und die Wikimedia Foundation ist (Fundraising). Der Spendenertrag ging in diesem Jahr auf 10,4 Mio. Euro (Vorjahr: 10,9 Mio. Euro) zurück. Das liegt darin begründet, dass die Spendenwerbung bei bekannten Spendenden in diesem Jahr durch den Verein und nicht mehr durch die WMFG durchgeführt wurde, weshalb diese Spenden direkt dem Verein zugute kamen. Die Personal- und Sachaufwendungen gingen auf 0,7 Mio. Euro (Vorjahr: 1,1 Mio. Euro) zurück. Damit konnten 6,8 Mio. Euro (Vorjahr: 7 Mio. Euro) an die Wikimedia Foundation in den USA und 2,6 Mio. Euro (Vorjahr: 3 Mio. Euro) an den Verein in Deutschland weitergeleitet werden. Die Aufteilung der Mittel orientiert sich an einer zwischen Wikimedia Deutschland und der Wiki­media Foundation geschlossenen Vereinbarung.</p>
+			<h2 class="h--gamma">Wirtschaftsprüfung</h2>
+			<p class="t--gamma-serif">Wikimedia Deutschland lässt interne Abläufe, die Spendenverteilung sowie die Vermögens-, Ertrags-, und Finanzlage freiwillig prüfen. Die Prüfung des Wikimedia Deutschland – Gesellschaft zur Förderung Freien Wissens e. V. und der Gemeinnützigen Wiki­media Fördergesellschaft mbH erfolgte durch die KWP Revision GmbH Wirtschaftsprüfungsgesellschaft Berlin und wurde im Dezember 2016 und Februar/März 2017 durchgeführt. Es wurde ein uneingeschränkter Bestätigungsvermerk erteilt. Demnach wurde die Buchführung des Vereins und der Gesellschaft vollständig und gewissenhaft vorgenommen und die Prüfung hat insgesamt zu keinerlei Einwänden geführt.</p>
+		</div>
+		<div class="limit--m finance__boxes">
+			<a class="border-box t--epsilon" href="<?= $url('finance/society', $lang) ?>">
+				<span>Zum Finanzteil <em class="t--delta">Wikimedia Deutschland e. V.</em></span>
+			</a>
+			<a class="border-box t--epsilon" href="<?= $url('finance/fund', $lang) ?>">
+				<span>Zum Finanzteil <em class="t--delta">Gemeinnützige Wikimedia Fördergesellschaft mbH</em></span>
+			</a>
+		</div>
+	</section>
+</main>
