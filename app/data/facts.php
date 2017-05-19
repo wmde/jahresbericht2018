dedd987f70174016fe14fe6c6246136055d449a1
Phillip Klein <phillip@atelierdisko.de>
2017-05-19 17:02:40 +0200

    Add review content.

--- app/data/facts.php
+++ app/data/facts.php
@@ -13,7 +13,7 @@
 			'de' => 'Videos wurden produziert, um den Einstieg für neue Wikipedia-Autorinnen und -Autoren zu erleichtern.',
 			'en' => ''
 		],
-		'icon' => '/assets/ico/neuautoren.svg',
+		'icon' => '/assets/ico/01_volunteers.svg',
 	],
 	[
 		'name' => '02',
@@ -29,7 +29,7 @@
 			'de' => 'Mal wurde das neue Portal zur Datenspende angeklickt.',
 			'en' => ''
 		],
-		'icon' => '/assets/ico/daten.svg',
+		'icon' => '/assets/ico/02_wikidata.svg',
 	],
 	[
 		'name' => '03',
@@ -45,7 +45,167 @@
 			'de' => 'Wikipedia-Sprachversionen (Deutsch, Arabisch, Hebräisch) verfügbar (sowie als optionales Feature in allen anderen Sprachen).',
 			'en' => ''
 		],
-		'icon' => '/assets/ico/techn_wunsch.svg',
+		'icon' => '/assets/ico/03_tech.svg',
+	],
+	[
+		'name' => '04',
+		'head' => [
+			'de' => '',
+			'en' => ''
+		],
+		'number' => [
+			'de' => '16.000',
+			'en' => ''
+		],
+		'tail' => [
+			'de' => 'Bildnachweise wurden mit dem Lizenzhinweisgenerator generiert.',
+			'en' => ''
+		],
+		'icon' => '/assets/ico/04_generator.svg',
+	],
+	[
+		'name' => '05',
+		'head' => [
+			'de' => '',
+			'en' => ''
+		],
+		'number' => [
+			'de' => '333',
+			'en' => ''
+		],
+		'tail' => [
+			'de' => 'Teilnehmende besuchten die WikiCon. Rekord für das größte Treffen von Wikipedia-Aktiven in Deutschland.',
+			'en' => ''
+		],
+		'icon' => '/assets/ico/05_wikicon.svg',
+	],
+	[
+		'name' => '06',
+		'head' => [
+			'de' => '',
+			'en' => ''
+		],
+		'number' => [
+			'de' => '10',
+			'en' => ''
+		],
+		'tail' => [
+			'de' => 'Forschende im Bereich Open Science ausgebildet.',
+			'en' => ''
+		],
+		'icon' => '/assets/ico/06_fellows.svg',
+	],
+	[
+		'name' => '07',
+		'head' => [
+			'de' => 'Gemeinsam mit',
+			'en' => ''
+		],
+		'number' => [
+			'de' => '13',
+			'en' => ''
+		],
+		'tail' => [
+			'de' => 'Partnerorganisationen setzten wir uns für freie Bildung ein.',
+			'en' => ''
+		],
+		'icon' => '/assets/ico/07_edu.svg',
+	],
+	[
+		'name' => '08',
+		'head' => [
+			'de' => '',
+			'en' => ''
+		],
+		'number' => [
+			'de' => '77',
+			'en' => ''
+		],
+		'tail' => [
+			'de' => 'Lernmaterialien wurden veröffentlicht, um den internationalen Austausch zu fördern.',
+			'en' => ''
+		],
+		'icon' => '/assets/ico/08_exchange.svg',
+	],
+	[
+		'name' => '09',
+		'head' => [
+			'de' => '',
+			'en' => ''
+		],
+		'number' => [
+			'de' => '1',
+			'en' => ''
+		],
+		'tail' => [
+			'de' => 'neues lokales Wikipedia-Büro eröffnete in München.',
+			'en' => ''
+		],
+		'icon' => '/assets/ico/09_rooms.svg',
+	],
+	[
+		'name' => '10',
+		'head' => [
+			'de' => 'Über',
+			'en' => ''
+		],
+		'number' => [
+			'de' => '200',
+			'en' => ''
+		],
+		'tail' => [
+			'de' => 'Menschen beteiligten sich an unserer Jahresplanung 2017.',
+			'en' => ''
+		],
+		'icon' => '/assets/ico/10_2017.svg',
+	],
+	[
+		'name' => '11',
+		'head' => [
+			'de' => '',
+			'en' => ''
+		],
+		'number' => [
+			'de' => '31.500',
+			'en' => ''
+		],
+		'tail' => [
+			'de' => 'exzellente Wikipedia-Artikel und -Listen machten sich für den Mond startklar.',
+			'en' => ''
+		],
+		'icon' => '/assets/ico/11_moon.svg',
+	],
+	[
+		'name' => '12',
+		'head' => [
+			'de' => 'Wir haben über',
+			'en' => ''
+		],
+		'number' => [
+			'de' => '15.000',
+			'en' => ''
+		],
+		'tail' => [
+			'de' => 'neue Mitgliedschaften für den Verein gewonnen.',
+			'en' => ''
+		],
+		'icon' => '/assets/ico/12_50k.svg',
+	],
+	[
+		'name' => '13',
+		'head' => [
+			'de' => 'Mit',
+			'en' => ''
+		],
+		'number' => [
+			'de' => '65.536',
+			'en' => ''
+		],
+		'tail' => [
+			'de' => 'Euro erhielt Wikimedia die bisher größte Einzelspende.',
+			'en' => ''
+		],
+		'icon' => '/assets/ico/13_finance.svg',
 	],
 
 ] ?>
