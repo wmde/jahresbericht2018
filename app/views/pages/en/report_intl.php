226e7c38eb3ab79135385d772414450d3052a024
Phillip Klein <phillip@atelierdisko.de>
2017-05-29 18:51:31 +0200

    Translate intl page.

--- app/views/pages/en/report_intl.php
+++ app/views/pages/en/report_intl.php
@@ -1,1 +1,33 @@
-Annual report: International exchange
+<main class="report-detail">
+	<section
+		class="hero cp cp--nav-zone"
+		style="background-image: url(/assets/img/08_intl_hero.jpg)"
+	>
+		<img class="hero__center-illu" src="/assets/ico/08_intl_white.svg" alt="Illustration" >
+		<div class="hero__filter"></div>
+	</section>
+	<section class="section cp report-detail__text">
+		<div class="hero__caption t--epsilon">Jason Krüger for Wikimedia Deutschland e.V. (<a href="https://commons.wikimedia.org/wiki/%20File:Wikimedia_Conference_2016_-_159.jpg" target="new">Wikimedia Commons</a>), <a href="https://creativecommons.org/licenses/by-sa/4.0/legalcode" target="new">CC BY-SA 4.0</a></div>
+		<div class="limit--s">
+			<h1 class="section__title h--beta">
+				<em>Wikimedia movement</em><br>Learning from each other in international partnerships
+			</h1>
+			<div class="section__teaser t--gamma">
+				Wikimedia Deutschland is part of an international network that is committed to spreading and promoting open knowledge throughout the world. Wikipedia and its sister projects are operated by the Wikimedia Foundation, based in San Francisco, California (USA). There are also more than a hundred organizations and groups around the world that promote and support the Wikimedia projects in their own language and region.
+			</div>
+			<div class="section__body t--gamma-serif">
+				<p>
+					Wikimedia Deutschland is the oldest and largest national organization: Over the twelve years of our existence, we have accumulated a lot of experience and knowledge. At the same time, we are certain that Wikimedia Deutschland could learn a lot from the other Wikimedia organizations. That is why we encourage our colleagues, but also volunteers via conferences, visits, and exchange programs to learn together with and from other Wikimedia contributors. Our goal is to establish learning partnerships and to thus enable constant exchange. In this way, for instance, we can share our experiences from our project for recruiting new authors for Wikipedia and learn more about what is being done in other countries to counteract the declining number of contributors.
+				</p>
+				<p>
+					Wikimedia Deutschland also takes a leading role in the international network. In 2016, we organized for the second year in a row the <a href="https://meta.wikimedia.org/wiki/Wikimedia_Conference_2016" target="new">Wikimedia Conference</a>. At the annual conference, Wikimedia organizations and groups, and the Wikimedia Foundation get together to discuss and determine the future direction of our global network. Simultaneously, the Wikimedia Conference is a meeting place for participants to learn from and to inspire each other and also to form new partnerships. The program is based on the specific wishes, experiences, and needs of the participants, and the topics of the conference are pursued and monitored throughout the entire year. By means of this approach, in 2016, we successfully laid the foundation for understanding and improving the relations between the Wikimedia Foundation and its organizations and groups.
+				</p>
+			</div>
+		</div>
+	</section>
+	<section class="section cp--center-section bg--icons-light-blue">
+		<div class="section__actions">
+			<a href="<?= $url('/report', $lang) ?>" class="button button--bg-green">Go to project overview</a>
+		</div>
+	</section>
+</main>
