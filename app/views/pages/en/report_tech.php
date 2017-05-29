294d67d890349bf4e2b892dd38ec964280551d5a
Phillip Klein <phillip@atelierdisko.de>
2017-05-29 17:46:11 +0200

    Translate tech page.

--- app/views/pages/en/report_tech.php
+++ app/views/pages/en/report_tech.php
@@ -1,1 +1,40 @@
-Annual report: Technical wishlist
+<main class="report-detail">
+	<section
+		class="hero cp cp--nav-zone"
+		style="background-image: url(/assets/img/03_tech_hero.jpg)"
+	>
+		<img class="hero__center-illu" src="/assets/ico/03_tech_white.svg" alt="Illustration" >
+		<div class="hero__filter"></div>
+	</section>
+	<section class="section cp report-detail__text">
+		<div class="hero__caption t--epsilon">Jan Dittrich (WMDE) (<a href="https://commons.wikimedia.org/wiki/File:2016-11-31_workshop_Search_Materials_Munich.jpg" target="new">Wikimedia Commons</a>), <a href="https://creativecommons.org/licenses/by-sa/4.0/legalcode" target="new">CC BY-SA 4.0</a></div>
+		<div class="limit--s">
+			<h1 class="section__title h--beta">
+				<em>Technical wishes</em><br>Wikimedia Deutschland meets community’s needs
+			</h1>
+			<div class="section__teaser t--gamma">
+				The software behind the Wikimedia projects must be continually developed and provided with new functions. Otherwise, its websites will soon no longer meet modern standards, frustrating users. The content will also become obsolete, since no one is maintaining it.
+			</div>
+			<div class="section__body t--gamma-serif">
+				<p>
+					Wikimedia Deutschland wants to help ensure that Wikipedia & Co. continue to be fun to use. That’s why we regularly ask contributors about the most important missing features or those in the most need of improvement. After the community discusses and prioritizes its "technical wishes," the team at Wikimedia Deutschland then determines which ones were feasible – it then implements them. The year 2016 was an incredibly productive year for the project, for many technical innovations could be realized. One major success was the release of the "<a href="https://mediawiki.org/wiki/Extension:RevisionSlider/en" target="new">RevisionSlider</a>“, which makes it possible to navigate between the different versions of an article. Collectively developed tools like this one or the similarly jointly developed <a href="http://tools.wmflabs.org/pageviews" target="new">analysis tool</a>, which shows the number of views of Wikipedia articles, are subsequently made available to Wikimedia projects in all languages. The collaborative nature of the “Technical Wishlist” project supports the common production of open knowledge.
+				</p>
+			</div>
+		</div>
+	</section>
+	<section class="section cp--center-section">
+		<div class="limit--s">
+			<div class="border-box border-box--green">
+				<div class="h--gamma">LINKS</div>
+				<ul class="bullet-list">
+					<li><a href="https://de.wikipedia.org/wiki/Wikipedia:Technische_Wünsche" target="new">Die Projektseite</a></li>
+				</ul>
+			</div>
+		</div>
+	</section>
+	<section class="section cp--center-section bg--icons-light-blue">
+		<div class="section__actions">
+			<a href="<?= $url('/report', $lang) ?>" class="button button--bg-green">Go to project overview</a>
+		</div>
+	</section>
+</main>
