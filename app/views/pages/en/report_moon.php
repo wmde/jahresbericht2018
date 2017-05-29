7805ce871e951a8adc92ad94362bd8937eee10ca
Phillip Klein <phillip@atelierdisko.de>
2017-05-29 19:11:57 +0200

    Translate moon page.

--- app/views/pages/en/report_moon.php
+++ app/views/pages/en/report_moon.php
@@ -1,1 +1,96 @@
-Annual report: Wikipedia to the Moon
+<main class="report-detail">
+	<section
+		class="hero cp cp--nav-zone"
+		style="background-image: url(/assets/img/11_moon_hero.jpg)"
+	>
+		<img class="hero__center-illu" src="/assets/ico/11_moon_white.svg" alt="Illustration" >
+		<div class="hero__filter"></div>
+	</section>
+	<section class="section cp report-detail__text">
+		<div class="hero__caption t--epsilon">Denis Schroeder (WMDE) (<a href="https://commons.wikimedia.org/wiki/File:Landemodul_ALINA_und_Lunar_Quattro_2.jpg" target="new">Wikimedia Commons</a>), <a href="https://creativecommons.org/licenses/by-sa/4.0/legalcode" target="new">CC BY-SA 4.0</a></div>
+		<div class="limit--s">
+			<h1 class="section__title h--beta">
+				<em>Wikipedia turned 15</em><br>... and is heading to the moon!
+			</h1>
+			<div class="section__teaser t--gamma">
+				An encyclopedia that anyone can edit? “The idea has no future, it can’t work ...” most people thought in 2001. Now, in 2016, Wikipedia is celebrating its 15th anniversary. Despite all the naysayers, an idea that was once frequently dismissed has become one of the world’s largest volunteer projects: Today, people from around the world read and edit about 44 million articles in nearly 300 languages.
+			</div>
+			<div class="section__body t--gamma-serif">
+				<p>
+					On Wikipedia’s 15th birthday, it received a very special gift. As the largest encyclopedia in history and a symbol of cooperation, Wikipedia has been selected to take a trip to the moon. After all, the heart and soul of the project is people sharing their knowledge with the whole world – and now far beyond.
+				</p>
+				<p>
+					The <a href="http://ptscientists.com/" target="new">PTScientists</a> from Berlin, who began as part-time researchers with the Google Lunar-X-Prize, aim to memorialize Wikipedia with their first purely privately funded lunar flight. As newcomers to the subject of space travel, the PTScientists acquired a great deal of their own knowledge from Wikipedia. As Robert Böhme, head of the PTScientists, recalls:  "For us, Wikipedia was an incredibly important source of knowledge at the beginning, because we’re really outsiders when it comes to space travel. It helped us to get started and to just learn the essentials – and to see what’s possible with an open exchange of information."
+				</p>
+				<p>
+					Since the encyclopedia now exceeds the limits of conventional data carriers, it was first necessary to decide which articles should be retained for the moon.  For the project, we were given a 20 gigabyte data disc, which is specially designed to survive travel into space.  In the first phase of the “Wikipedia to the Moon” project, the international Wikipedia community discussed which of the millions of articles should be preserved. One suggestion was to send the Wikipedia article on the moon in all the languages of the world. Another proposal that found support was to send all the articles that would make it possible for extraterrestrials to reconstruct human DNA.  On the basis of the excellent articles and lists from all language versions, the community finally decided to go with the very best articles from Wikipedia as a time capsule for posterity. Following an extensive evaluation process, the Wikipedia community determines these articles to have the highest quality and to be the most comprehensive.  From July through October, Wikipedia authors from all over the world then worked on their favorite articles to make them even better and to qualify them for the moon data disc.  As a result, more than 450 new featured articles were produced within four months, 19 of which came from Germany. The Munich Olympic stadium will now be immortalized on the moon right along with Dalí's mustache.
+				</p>
+				<blockquote class="quote">
+					<div class="quote__body">
+						<p class="t--beta">
+							"What is really great about the project is that the work of the many thousands of people who made Wikipedia what it is today is actually going to land on the moon."
+						</p>
+					</div>
+					<footer class="quote__author t--gamma">
+					Michael Jahn, spokesperson for the “Wikipedia to the Moon” project at Wikimedia Deutschland
+					</footer>
+				</blockquote>
+				<p>
+					Wikimedia Deutschland supported this process communicatively and logistically. We wanted to utilize the project to draw attention to the possibility of collaborating in Wikipedia.  At the project’s closing event on December 5, 2016, which was also International Volunteer Day, the PTScientists visited Wikimedia Deutschland with their ALINA landing module and a moon rover.   They received the Wikipedia disc and answered questions about their mission to the moon.  The data carrier is expected to be attached to the landing module and shot to the moon in early 2018. It will remain there as a time capsule for at least 1,000 years. Along with the articles themselves, the user names of the respective authors will also be deposited on the moon to honor their commitment as digital volunteers.
+				</p>
+			</div>
+		</div>
+	</section>
+	<section class="section cp--center-section">
+		<div class="limit--l">
+			<div class="section__body slider swiper-container loading">
+				<div class="swiper-wrapper">
+					<div class="swiper-slide">
+						<figure class="fig">
+							<img class="fig__image" src="/assets/img/11_moon_scientists.jpg" alt="Die Part-Time Scientists mit der Wikipedia-Datendisc">
+							<figcaption class="fig__caption limit--s">
+								Jan Apel (WMDE) <a href="https://commons.wikimedia.org/wiki/File:Die_Part-Time_Scientists_mit_der_Wikipedia-Datendisc.jpg" target="new">(Wikimedia Commons)</a>, <a href="https://creativecommons.org/licenses/by-sa/4.0/legalcode" target="new">CC BY 4.0</a>
+							</figcaption>
+						</figure>
+					</div>
+					<div class="swiper-slide">
+						<figure class="fig">
+							<img class="fig__image" src="/assets/img/11_moon_lander.jpg" alt="Landemodul ALINA mit Wikipedia-Datendisc">
+							<figcaption class="fig__caption limit--s">
+								Denis Schroeder (WMDE) <a href="https://commons.wikimedia.org/wiki/File:Landemodul_ALINA_mit_Wikipedia-Datendisc.jpg" target="new">(Wikimedia Commons)</a>, <a href="https://creativecommons.org/licenses/by-sa/4.0/legalcode" target="new">CC BY 4.0</a>
+							</figcaption>
+						</figure>
+					</div>
+					<div class="swiper-slide">
+						<div class="video">
+							<div class="video__embed-container">
+								<iframe src="https://player.vimeo.com/video/196444225?byline=0&title=0&portrait=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+							</div>
+							<div class="video__caption limit--s">
+								Wikipedia to the Moon Projektrückblick
+								<a href="https://creativecommons.org/licenses/by-sa/3.0/" target="new">CC BY-SA 3.0</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="swiper-button-next swiper-button-next--green"></div>
+				<div class="swiper-button-prev swiper-button-prev--green"></div>
+			</div>
+		</div>
+	</section>
+	<section class="section cp--center-section">
+		<div class="limit--s">
+			<div class="border-box border-box--green">
+				<div class="h--gamma">LINKS</div>
+				<ul class="bullet-list">
+					<li><a href="https://meta.wikimedia.org/wiki/Wikipedia_to_the_Moon/de" target="new">Internationale Projektseite</a></li>
+				</ul>
+			</div>
+		</div>
+	</section>
+	<section class="section cp--center-section bg--icons-light-blue">
+		<div class="section__actions">
+			<a href="<?= $url('/report', $lang) ?>" class="button button--bg-green">Go to project overview</a>
+		</div>
+	</section>
+</main>
