48354cdf19b14e5bc19dc81b296147ff12872615
Phillip Klein <phillip@atelierdisko.de>
2017-05-29 18:22:07 +0200

    Translate wikicon page.

--- app/views/pages/en/report_wikicon.php
+++ app/views/pages/en/report_wikicon.php
@@ -1,1 +1,49 @@
-Annual report: WikiCon
+<main class="report-detail">
+	<section
+		class="hero cp cp--nav-zone"
+		style="background-image: url(/assets/img/05_wikicon_hero.jpg)"
+	>
+		<img class="hero__center-illu" src="/assets/ico/05_wikicon_white.svg" alt="Illustration" >
+		<div class="hero__filter"></div>
+	</section>
+	<section class="section cp report-detail__text">
+		<div class="hero__caption t--epsilon">Martin Kraft (<a href="https://commons.wikimedia.org/wiki/File:MJK09805_Gruppenbild_WikiCON_2016.jpg" target="new">Wikimedia Commons</a>), „MJK09805 Gruppenbild WikiCON 2016“, <a href="https://creativecommons.org/licenses/by-sa/4.0/legalcode" target="new">CC BY-SA 4.0</a></div>
+		<div class="limit--s">
+			<h1 class="section__title h--beta">
+				<em>Association and communities</em><br>The largest WikiCon in Kornwestheim
+			</h1>
+			<div class="section__teaser t--gamma">
+				At Wikipedia, collaboration does not mean writing an article at your computer by yourself. The Wikipedia community is a community that also meets regularly in the real world. There’s no other event like the WikiCon where so many Wikipedians come together. It is the biggest German-language conference for Wikimedia project volunteers and takes place once a year at different locations. In 2016, it was held in the region of Stuttgart.
+			</div>
+			<div class="section__body t--gamma-serif">
+				<p>
+					The WikiCon is organized by volunteers for volunteers. Supporting the organizing team are the employees of Wikimedia Deutschland, Wikimedia Österreich, and Wikimedia Schweiz. In addition to project presentations, and lectures and discussions on all things relating to the Wikimedia projects, there were plenty of chances in September for members to get to know one another and to collectively work on new ideas. The "<a href="https://de.wikipedia.org/wiki/Wikipedia:Wiki_Loves_Cocktails" target="new">Wiki Loves Cocktails</a>" evening project offered ample opportunity for this. After all, it would have been a shame to not take advantage of all the great articles and pictures of delicious drinks that had been produced!
+				</p>
+				<blockquote class="quote">
+					<div class="quote__body">
+						<p class="t--beta">
+							"The WikiCon is the perfect opportunity to meet other contributors whom, for many years, I only knew virtually.”
+						</p>
+					</div>
+					<footer class="quote__author t--gamma">
+						WikiCon participant
+					</footer>
+				</blockquote>
+				<p>
+					In 2016, the organizing committee took the concept of the conference one step further. What was previously only a meeting for active volunteers, now welcomed interested newcomers to learn about Wikipedia and other Wikimedia projects during a portion of the conference that was open to the public. Also for the first time, there were lectures from external experts in education policy and science, as well as from the Open Knowledge Foundation Deutschland and the OpenStreetMap project.
+				</p>
+				<p>
+					With 333 participants, the 2016 edition of WikiCon in Baden-Württemberg’s Kornwestheim was the biggest and most successful yet. It succeeded in communicating how much the commitment of the volunteers is appreciated: For 93 percent of respondents of the final <a href="https://de.wikipedia.org/wiki/Wikipedia:WikiCon_2016/Evaluation" target="new">survey</a>, the conference reinforced their motivation to contribute to Wikipedia and its sister projects.
+				</p>
+				<p>
+					We look forward to the <a href="https://de.wikipedia.org/wiki/Wikipedia:WikiCon_2017" target="new">next WikiCon</a> in Leipzig from September 8-10!
+				</p>
+			</div>
+		</div>
+	</section>
+	<section class="section cp--center-section bg--icons-light-blue">
+		<div class="section__actions">
+			<a href="<?= $url('/report', $lang) ?>" class="button button--bg-green">Go to project overview</a>
+		</div>
+	</section>
+</main>
