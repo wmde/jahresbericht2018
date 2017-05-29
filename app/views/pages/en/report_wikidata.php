adf45e60dc7d295a0cccf54af1b5d6a4563753de
Phillip Klein <phillip@atelierdisko.de>
2017-05-29 17:05:28 +0200

    Translate wikidata page.

--- app/views/pages/en/report_wikidata.php
+++ app/views/pages/en/report_wikidata.php
@@ -1,1 +1,76 @@
-Annual report: Wikidata is looking for data partnerships
+<main class="report-detail">
+	<section
+		class="hero cp cp--nav-zone"
+		style="background-image: url(/assets/img/02_wikidata_hero.jpg)"
+	>
+		<img class="hero__center-illu" src="/assets/ico/02_wikidata_white.svg" alt="Illustration" >
+		<div class="hero__filter"></div>
+	</section>
+	<section class="section cp report-detail__text">
+		<div class="hero__caption t--epsilon">Atelier Disko (atelierdisko.de) (<a href="https://commons.wikimedia.org/wiki/File:PanamaPapers_Grafik.svg" target="new">Wikimedia Commons</a>), <a href="https://creativecommons.org/licenses/by-sa/4.0/legalcode" target="new">CC BY-SA 4.0</a></div>
+		<div class="limit--s">
+			<h1 class="section__title h--beta">
+				<em>Wikidata</em><br>Open database seeks data partnerships
+			</h1>
+			<div class="section__teaser t--gamma">
+				Wikipedia contains a lot of information. It is structured, however, like an encyclopedia in the form of articles. For instance, if you’re looking for the professions of people who are connected to the Panama Papers, this information can naturally be found in various Wikipedia articles. Yet it is not available with a single click. Wikidata, as an open database of the world's knowledge, can provide such information much faster.
+			</div>
+			<div class="section__body t--gamma-serif">
+				<p>
+					Wikimedia Deutschland launched Wikidata in the fall of 2012. Since then, more than 33,000 active users have created around 24 million entries and filled them with data.
+				</p>
+				<p>
+					If a researcher is currently working on examining the people named in the Panama Papers, they can enter their results into Wikidata. The information is then made available worldwide. Since the data is freely accessible, everyone can use it without having to request permission or pay.
+				</p>
+				<p>
+					With Wikidata, Wikimedia wants to give more people more access to more knowledge. In 2016, we focused on freeing the knowledge of institutions. We advocate for information that was previously only available in archives and museums  to be digitized and made accessible to the entire world.
+				</p>
+				<p>
+					Whether it’s the years of origin of paintings, names of artists, or the latest exhibition catalog – the knowledge of an institution thus is not confined to a single site, but is available worldwide.
+				</p>
+				<p>
+					By the same token, we are not interested in accumulating as much content as possible or just any content in Wikidata. We are concentrating on partnerships with reputable institutions. In 2016, we created a process for them to easily share their data with one of the world's largest websites. Along with John Cummings, "Wikimedian in residence” at UNESCO, we were able to identify all the practical pitfalls in the data-partnership process. Here, we recognized the actual complexity of the process and included every finding in a comprehensive documentation. A portal for data donations was also developed. It has already been translated into numerous languages and explains the individual steps for making a successful data donation. The TED organization (short for Technology, Entertainment, Design) has already started to enter the data on its educational TED talks into Wikidata with the help of two "Wikipedians in residence.”
+				</p>
+				<p>
+					With the development of a clear data donation process, we have gone a long way towards our goal of explaining to institutions exactly why open data is a great thing and why they should make theirs available.
+				</p>
+			</div>
+		</div>
+	</section>
+	<section class="section cp--center-section">
+		<div class="limit--l">
+			<div class="section__body slider swiper-container loading">
+				<div class="swiper-wrapper">
+					<div class="swiper-slide">
+						<div class="video">
+							<div class="video__embed-container">
+								<iframe src="https://player.vimeo.com/video/198330871?byline=0&title=0&portrait=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+							</div>
+							<div class="video__caption limit--s">
+								Donating data to Wikidata
+								<a href="https://creativecommons.org/licenses/by-sa/3.0/" target="new">CC BY-SA 3.0</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="swiper-button-next swiper-button-next--green"></div>
+				<div class="swiper-button-prev swiper-button-prev--green"></div>
+			</div>
+		</div>
+	</section>
+	<section class="section cp--center-section">
+		<div class="limit--s">
+			<div class="border-box border-box--green">
+				<div class="h--gamma">LINKS</div>
+				<ul class="bullet-list">
+					<li><a href="https://www.wikidata.org/wiki/Wikidata:Data_donation" target="new">Portal zur Datenspende</a></li>
+				</ul>
+			</div>
+		</div>
+	</section>
+	<section class="section cp--center-section bg--icons-light-blue">
+		<div class="section__actions">
+			<a href="<?= $url('/report', $lang) ?>" class="button button--bg-green">Go to project overview</a>
+		</div>
+	</section>
+</main>
