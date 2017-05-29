a25f75d3dee9fb0ad83492e5e487328ff2b47061
Phillip Klein <phillip@atelierdisko.de>
2017-05-29 19:01:11 +0200

    Translate rooms page.

--- app/views/pages/en/report_rooms.php
+++ app/views/pages/en/report_rooms.php
@@ -1,1 +1,43 @@
-Annual report: Local rooms
+<main class="report-detail">
+	<section
+		class="hero cp cp--nav-zone"
+		style="background-image: url(/assets/img/09_rooms_hero.jpg)"
+	>
+		<img class="hero__center-illu" src="/assets/ico/09_rooms_white.svg" alt="Illustration" >
+		<div class="hero__filter"></div>
+	</section>
+	<section class="section cp report-detail__text">
+		<div class="hero__caption t--epsilon">Atelier Disko (atelierdisko.de) (<a href="https://commons.wikimedia.org/wiki/File:%C3%9Cbersicht_LokaleRaeume.svg" target="new">Wikimedia Commons</a>), <a href="https://creativecommons.org/licenses/by-sa/4.0/legalcode" target="new">CC BY-SA 4.0</a></div>
+		<div class="limit--s">
+			<h1 class="section__title h--beta">
+				<em>Regionalization</em><br>Local spaces and hubs give Wikimedia projects a face
+			</h1>
+			<div class="section__teaser t--gamma">
+				Many Wikimedia project volunteers also want to get together outside of the digital world. As a consequence, regular <a href="https://de.wikipedia.org/wiki/Wikipedia:Treffen_der_Wikipedianer" target="new">Wikipedia “Stammtische”</a> (reserved tables for participants) and editing sessions have already been routinely taking place at various locations across Germany since 2003. Several cities now have volunteer-based community-led offices where volunteers can meet for joint editing sessions or events.
+			</div>
+			<div class="section__body t--gamma-serif">
+				<p>
+					Volunteers in <a href="https://de.wikipedia.org/wiki/Wikipedia:WikiProjekt_Bremen/BremenpediA/vor_Ort" target="new">Bremen</a>, <a href="https://de.wikipedia.org/wiki/Wikipedia:Kontor_Hamburg" target="new">Hamburg</a>, <a href="https://de.wikipedia.org/wiki/Wikipedia:Wikipedia-B%C3%BCro_Hannover" target="new">Hanover</a>, <a href="https://de.wikipedia.org/wiki/Wikipedia:Lokal_K" target="new">Cologne</a> und <a href="https://de.wikipedia.org/wiki/Wikipedia:WikiMUC" target="new">Munich</a> (as well as in Berlin starting in 2017) meet in local spaces to plan shared projects and to grow together as a community locally. At the same time, they exchange ideas with other organizations that have similar areas of focus and are available for collaborations with regional institutions. In the process, new solutions, projects, and ideas develop that they implement themselves or with the support of Wikimedia Deutschland. The <a href="https://de.wikipedia.org/wiki/Wikipedia:WikiMUC" target="new">WikiMUC</a> space, which opened in Munich in the spring of 2016, is already active at the regional level with Serlo (an open-learning platform), Kiron (higher education for refugees) and Start2Code (where kids learn computer programming).
+				</p>
+				<p>
+					Wikimedia Deutschland supports the globally unique concept of local spaces as a logistical and financial partner. This includes contract negotiations and payments for rent, utilities, and operating costs as well as funding for equipment, events, and public relations. The volunteers, as a result, can concentrate entirely on their activities. Wikimedia Deutschland regularly exchanges with them at events about the local work and supports them in evaluating it. In 2016, a total of 489 activities were conducted in local spaces and hubs. Among these, many events brought together volunteers, disseminated information about the Wikimedia projects, or actively contributed to them.
+				</p>
+			</div>
+		</div>
+	</section>
+	<section class="section cp--center-section">
+		<div class="limit--s">
+			<div class="border-box border-box--green">
+				<div class="h--gamma">LINKS</div>
+				<ul class="bullet-list">
+					<li><a href="https://de.wikipedia.org/wiki/Wikipedia:Förderung/Lokale_Community-Räume" target="new">Mehr Infos zu den lokalen Räumen</a></li>
+				</ul>
+			</div>
+		</div>
+	</section>
+	<section class="section cp--center-section bg--icons-light-blue">
+		<div class="section__actions">
+			<a href="<?= $url('/report', $lang) ?>" class="button button--bg-green">Go to project overview</a>
+		</div>
+	</section>
+</main>
