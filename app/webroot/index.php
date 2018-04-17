96f28e408a08ea4e555e92d0520a917a1d122499
Marius Wilms <mariuswilms@gmx.de>
2018-04-17 12:00:42 +0200

    Continue refactor, fix finance routing
    - hasBlackHeader becomes part of route handlers

--- app/webroot/index.php
+++ app/webroot/index.php
@@ -11,6 +11,10 @@
 define('PROJECT_APP_PATH', __DIR__);
 define('PROJECT_DOMAIN', 'wmde-bericht2017.test');
 
+$routes = [];
+$helpers = [];
+$viewVars = [];
+
 // Variables that are always available in views.
 $lang = 'de';
 $path = $_SERVER['REQUEST_URI'];
@@ -23,53 +27,47 @@ if (preg_match('#^/(de|en)(/.*)?#', $path, $matches)) {
 	$path = '/' . ltrim($matches[2], '/');
 }
 
+//
+// Helpers
+//
+
 // Used to construct URLs in the view.
-$url = function($goal, $lang) {
+$helpers['url'] = function($goal, $lang) {
 	$path = $lang . '/' . trim($goal, '/');
 	return '/' . trim($path, '/');
 };
 
 // Used to construct a URL for switching the language, but
 // staying on the same page.
-$switchLanguageFromUrl = function($lang) use ($url) {
-	return $url($lang === 'de' ? 'en' : 'de');
+$helpers['switchLanguageFromUrl'] = function($lang) use ($helpers) {
+	return $helpers['url']($lang === 'de' ? 'en' : 'de');
 };
 
-$viewFileFromURI = function($path, $lang) {
-	// $path may contain query string
-	$path = parse_url($path, PHP_URL_PATH);
-
-	$viewBase = PROJECT_APP_PATH . '/views/pages/' . $lang;
-	$viewName = str_replace('/', '_', trim($path, '/'));
-
-	$viewFile = $viewBase . '/' . $viewName . '.php';
-	$viewFile = realpath($viewFile);
-
-	if ($viewName === '') {
-		return $viewBase . '/home.php';
-	}
-
-	// Prevent directory traversal attack
-	// see http://stackoverflow.com/a/4205278
-	if ($viewFile === false || strpos($viewFile, $viewBase) !== 0) {
-		return false;
-	}
-	return $viewFile;
-};
-
-// Match incoming request to routes and invoke the corresponding handler,
-// once a route matched.
-$routes = [];
+//
+// Routes
+//
 
 $routes['#^/$#'] = function() {
 	$reports = require PROJECT_APP_PATH .'/data/reports.php';
 	$facts = require PROJECT_APP_PATH .'/data/facts.php';
 	$fact = $facts[array_rand($facts)];
-	return compact('reports', 'facts', 'fact');
+
+	return compact('reports', 'facts', 'fact') + [
+		'hasBlackHeader' => false
+	];
+};
+$routes['#^/finance/fund$#'] = function() {
+	return ['hasBlackHeader' => true];
+};
+$routes['#^/finance#'] = function() {
+	return ['hasBlackHeader' => false];
 };
 $routes['#^/review$#'] = function() {
 	$facts = require PROJECT_APP_PATH .'/data/facts.php';
-	return compact('facts');
+
+	return compact('facts') + [
+		'hasBlackHeader' => false
+	];
 };
 $routes['#^/report$#'] = function($matches, $query) {
 	$reports = require PROJECT_APP_PATH .'/data/reports.php';
@@ -79,7 +77,9 @@ $routes['#^/report$#'] = function($matches, $query) {
 			return $v['category'] === $query['filter'];
 		});
 	}
-	return compact('reports');
+	return compact('reports') + [
+		'hasBlackHeader' => true
+	];
 };
 $routes['#^/report/(.*)$#'] = function($matches) {
 	$reports = require PROJECT_APP_PATH .'/data/reports.php';
@@ -91,39 +91,77 @@ $routes['#^/report/(.*)$#'] = function($matches) {
 			break;
 		}
 	}
-	return compact('report');
+	return compact('report') + [
+		'hasBlackHeader' => true
+	];
 };
 
-$routeMatched = false;
-foreach ($routes as $regex => $handler) {
-	if (preg_match($regex, parse_url($path, PHP_URL_PATH), $matches)) {
-		$query = parse_url($path, PHP_URL_QUERY);
-		parse_str($query, $query);
+//
+// Functions
+//
 
-		extract($handler($matches, $query), EXTR_SKIP);
+// Matches current request against registered routes and
+// returns view variables.
+$matchRoute = function($path, $routes) {
+	$query = parse_url($path, PHP_URL_QUERY);
+	parse_str($query, $query);
 
-		$routeMatched = true;
-		break;
+	$path = parse_url($path, PHP_URL_PATH);
+
+	foreach ($routes as $regex => $handler) {
+		if (preg_match($regex, $path, $matches)) {
+			return $handler($path, $query, $matches);
+		}
 	}
-}
-if (!$routeMatched) {
+	return false;
+};
+
+$renderView = function($viewVars, $helpers, $path, $lang) {
+	$viewFileFromURI = function($path, $lang) {
+		// $path may contain query string
+		$path = parse_url($path, PHP_URL_PATH);
+
+		$viewBase = PROJECT_APP_PATH . '/views/pages/' . $lang;
+		$viewName = str_replace('/', '_', trim($path, '/'));
+
+		$viewFile = $viewBase . '/' . $viewName . '.php';
+		$viewFile = realpath($viewFile);
+
+		if ($viewName === '') {
+			return $viewBase . '/home.php';
+		}
+
+		// Prevent directory traversal attack
+		// see http://stackoverflow.com/a/4205278
+		if ($viewFile === false || strpos($viewFile, $viewBase) !== 0) {
+			return false;
+		}
+		return $viewFile;
+	};
+	$viewFile = $viewFileFromURI($path, $lang);
+
+	if ($viewFile === false || trim($path, '/') === 'home') {
+		return false;
+	}
+	extract($helpers, EXTR_SKIP);
+	extract($viewVars, EXTR_SKIP);
+
+	require PROJECT_APP_PATH . '/views/elements/' . $lang . '/header.php';
+	require $viewFile;
+	require PROJECT_APP_PATH . '/views/elements/' . $lang . '/footer.php';
+};
+
+//
+// Handle the request
+//
+
+if (($viewVars = $matchRoute($path, $routes)) === false) {
 	header('HTTP/1.1 404 Not Found');
 	exit();
 }
-
-// Some pages will need an inverted header.
-$hasBlackHeader = (boolean) preg_match('#^(/report/?|/finance/fund)$#', $path);
-
-// Render the template as a view.
-$viewFile = $viewFileFromURI($path, $lang);
-
-if ($viewFile === false || trim($path, '/') === 'home') {
+if ($renderView($viewVars, $helpers, $path, $lang) === false) {
 	header('HTTP/1.1 404 Not Found');
 	exit();
 }
-
-require PROJECT_APP_PATH . '/views/elements/' . $lang . '/header.php';
-require $viewFile;
-require PROJECT_APP_PATH . '/views/elements/' . $lang . '/footer.php';
 
 ?>
