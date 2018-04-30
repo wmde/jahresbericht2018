3ef1520d968f5ae19e3e38fa29c8a7e50b8d5197
Franca Winter <franca@atelierdisko.de>
2018-04-30 13:08:35 +0200

    Update report filter texts

--- app/webroot/data/report_filters.php
+++ app/webroot/data/report_filters.php
@@ -6,7 +6,7 @@
 			'en' => 'Wikimedia'
 		],
 		'teaser' => [
-			'de' => 'Als Gesellschaft zur Förderung Freien Wissens arbeitet Wikimedia Deutschland daran, dass Projekte wie die Wikipedia auch in Zukunft existieren können und dass Freies Wissen in unserer Gesellschaft zur Selbstverständlichkeit wird. Wir unterstützen die unschätzbar wertvolle Arbeit der Ehrenamtlichen in den Wikimedia-Projekten und wirken darauf hin, dass sich noch mehr Menschen beteiligen. Wir entwickeln und verbessern Software, damit die Freiwilligen ihr Wissen noch besser in den Wikimedia-Projekten teilen können. Wir arbeiten mit Institutionen aus Kultur, Bildung und Wissenschaft zusammen und bringen politischen und gesellschaftlichen Entscheidungsträgern die Idee des Freien Wissens näher.',
+			'de' => 'Als Gesellschaft zur Förderung Freien Wissens arbeitet Wikimedia Deutschland daran, dass Projekte wie die Wikipedia auch in Zukunft existieren können und dass Freies Wissen in unserer Gesellschaft zur Selbstverständlichkeit wird. Wir unterstützen die unschätzbar wertvolle Arbeit der Ehrenamtlichen in den Wikimedia-Projekten und wirken darauf hin, dass sich noch mehr Menschen beteiligen. Wir entwickeln und verbessern Software, damit Freiwillige ihr Wissen noch besser in den Wikimedia-Projekten teilen können. Wir arbeiten mit Institutionen aus Kultur, Bildung und Wissenschaft zusammen und bringen politischen und gesellschaftlichen Entscheidungsträgern die Idee des Freien Wissens näher.',
 			'en' => 'English text'
 		]
 	],
@@ -17,7 +17,7 @@
 			'en' => ''
 		],
 		'teaser' => [
-			'de' => 'Als Gesellschaft zur Förderung Freien Wissens arbeitet Wikimedia Deutschland daran, dass Projekte wie die Wikipedia auch in Zukunft existieren können und dass Freies Wissen in unserer Gesellschaft zur Selbstverständlichkeit wird. Wir unterstützen die unschätzbar wertvolle Arbeit der Ehrenamtlichen in den Wikimedia-Projekten und wirken darauf hin, dass sich noch mehr Menschen beteiligen. Wir entwickeln und verbessern Software, damit die Freiwilligen ihr Wissen noch besser in den Wikimedia-Projekten teilen können. Wir arbeiten mit Institutionen aus Kultur, Bildung und Wissenschaft zusammen und bringen politischen und gesellschaftlichen Entscheidungsträgern die Idee des Freien Wissens näher.',
+			'de' => 'Ehrenamtliche Autorinnen und Autoren teilen in der freien Enzyklopädie Wikipedia kostenlos ihr Wissen, indem sie Fehler verbessern, Fotos einbinden, Informationen aktuell halten und ganze Artikel schreiben. Ohne den unermüdlichen Einsatz von vielen Tausend Freiwilligen würden Projekte wie die Wikipedia nicht existieren. Deswegen ist die Förderung der Freiwilligen ein fester Bestandteil der Arbeit von Wikimedia Deutschland. 2017 haben wir beispielsweise zahlreiche Aktiven-Treffen ermöglicht – bei der größten davon, der WikiCon, kamen 287 Ehrenamtliche in Leipzig zusammen. Wir unterstützen auch die Einrichtung lokaler Wikipedia-Büros, die mittlerweile in Köln, Hamburg, München und seit 2017 auch in Berlin die Arbeit der Freiwilligen erleichtern. Ein großer Schwerpunkt waren 2017 zudem die vier Banner-Kampagnen innerhalb der Wikipedia, mit denen wir neue Autorinnen und Autoren für die freie Enzyklopädie gewinnen wollten.',
 			'en' => 'English text'
 		]
 	],
@@ -28,7 +28,7 @@
 			'en' => ''
 		],
 		'teaser' => [
-			'de' => 'Als Gesellschaft zur Förderung Freien Wissens arbeitet Wikimedia Deutschland daran, dass Projekte wie die Wikipedia auch in Zukunft existieren können und dass Freies Wissen in unserer Gesellschaft zur Selbstverständlichkeit wird. Wir unterstützen die unschätzbar wertvolle Arbeit der Ehrenamtlichen in den Wikimedia-Projekten und wirken darauf hin, dass sich noch mehr Menschen beteiligen. Wir entwickeln und verbessern Software, damit die Freiwilligen ihr Wissen noch besser in den Wikimedia-Projekten teilen können. Wir arbeiten mit Institutionen aus Kultur, Bildung und Wissenschaft zusammen und bringen politischen und gesellschaftlichen Entscheidungsträgern die Idee des Freien Wissens näher.',
+			'de' => 'Wikimedia Deutschland unterstützt nicht nur die Ehrenamtlichen, die in den Wikimedia-Projekten ihr Wissen mit der Welt teilen, sondern kümmert sich auch um die stetige Verbesserung der technischen Infrastruktur für Wikipedia & Co. Das beinhaltet beispielsweise die Arbeit an der Software, mit der sämtliche Wikimedia-Projekte betrieben werden. Darüber hinaus entwickeln wir im engen Austausch mit Freiwilligen technische Produkte zur Weiterentwicklung von Wikipedia und zur Verbreitung Freien Wissens. Ein Großprojekt, an dem wir Hand in Hand mit einer internationalen Community arbeiten, ist die freie, kollaborativ gepflegte Wissensdatenbank Wikidata, die 2012 bei Wikimedia Deutschland in Berlin startete und seitdem federführend durch uns ausgebaut und weiterentwickelt wird. Unsere Arbeit im Bereich der Software-Entwicklung folgt dem Ziel, dem sich auch die Tausenden von Wikimedia-Aktiven verschrieben haben: mehr Menschen mehr Zugang zu mehr Wissen zu ermöglichen.',
 			'en' => 'English text'
 		]
 	],
@@ -39,7 +39,7 @@
 			'en' => ''
 		],
 		'teaser' => [
-			'de' => 'Als Gesellschaft zur Förderung Freien Wissens arbeitet Wikimedia Deutschland daran, dass Projekte wie die Wikipedia auch in Zukunft existieren können und dass Freies Wissen in unserer Gesellschaft zur Selbstverständlichkeit wird. Wir unterstützen die unschätzbar wertvolle Arbeit der Ehrenamtlichen in den Wikimedia-Projekten und wirken darauf hin, dass sich noch mehr Menschen beteiligen. Wir entwickeln und verbessern Software, damit die Freiwilligen ihr Wissen noch besser in den Wikimedia-Projekten teilen können. Wir arbeiten mit Institutionen aus Kultur, Bildung und Wissenschaft zusammen und bringen politischen und gesellschaftlichen Entscheidungsträgern die Idee des Freien Wissens näher.',
+			'de' => 'Die Stärkung rechtlicher und politischer Rahmenbedingungen für Freies Wissen auf EU- und Bundesebene spielt für Wikimedia Deutschland eine zentrale Rolle. 2017 konnten wir Politikerinnen und Politiker durch direkte Gespräche, Veranstaltungen, Stellungnahmen und verschiedene Materialien auf unsere Positionen aufmerksam machen und die Vorteile Freien Wissens für die Gesellschaft vermitteln. Außerdem haben wir mit Kultur-, Bildungs- und Wissenschaftsinstitutionen zusammengearbeitet, um mehr Inhalte für die Wikimedia-Projekte &mdash; und damit für alle Menschen &mdash; zu befreien. Im Bildungsbereich haben wir 2017 weiterhin das Thema freie Bildungsmaterialien (englisch: Open Educational Resources, kurz OER) vorangebracht.',
 			'en' => 'English text'
 		]
 	],
