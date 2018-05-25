4953785ba9f8c2bf140a1b6d9fd29856d8c74fdc
Franca Winter <franca@atelierdisko.de>
2018-05-25 11:28:28 +0200

    Change imprint content for GDPR

--- app/webroot/views/elements/de/footer.php
+++ app/webroot/views/elements/de/footer.php
@@ -32,7 +32,7 @@
 	</div>
 	<div class="main-footer__bottom clearfix">
 		<div class="limit--l">
-			<a class="imprint-toggle nav__link" href="#imprint-toggle">Impressum</a>
+			<a class="imprint-toggle nav__link" href="#imprint-toggle">Impressum und Datenschutz</a>
 			<a class="imprint-close image-replace invis" href="#imprint-close">Schließen</a>
 			<article id="imprint" class="imprint invis cp--2x-bottom">
 				<h1 class="h--delta">
@@ -61,40 +61,7 @@
 				<section>
 					<h1 class="h--delta">Datenschutzerklärung</h1>
 					<p class="t--delta-serif">
-						Personenbezogene Daten werden auf dieser Webseite nur im technisch notwendigen Umfang erhoben. In keinem Fall werden die erhobenen Daten verkauft oder aus anderen Gründen an Dritte weitergegeben. Im Folgenden erhalten Sie einen Überblick über die von uns im Rahmen dieser Website erhobenen Daten.
-					</p>
-					<p class="t--delta-serif">
-						Log-Files: Bei der Nutzung dieses Angebots werden die von Ihrem Browser an den Server übermittelten Daten erfasst und gespeichert. Dies umfasst üblicherweise Informationen über den Typ und die Version des von Ihnen verwendeten Browsers, das verwendete Betriebssystem, die Referrer URL (die Webseite, von der aus Sie zu dieser Website gelangt sind), den Hostnamen des zugreifenden Rechners (die IP Adresse) sowie die Uhrzeit der Serveranfrage. In der Regel lassen sich diese Daten nicht bestimmten Personen – und damit auch nicht Ihnen – zuordnen. Weder wird zu diesem Zweck ein Abgleich der Daten mit anderen Daten vorgenommen, noch werden sie hierfür mit anderen Daten zusammengeführt. Die Daten werden regelmäßig nach einer statistischen Auswertung gelöscht.
-					</p>
-					<p class="t--delta-serif">
-						Cookies: Diese Website verwendet an mehreren Stellen Cookies. Cookies sind kleine Textdateien, die Ihr Browser speichert und die dazu dienen, die Benutzung einer Website einfacher, effektiver und sicherer zu machen. Cookies richten auf Ihrem Rechner keinen Schaden an, nehmen minimalen Speicherplatz in Anspruch und enthalten keine Viren.
-					</p>
-					<p class="t--delta-serif">
-						Google Analytics: Diese Website benutzt Google Analytics, einen Webanalysedienst der Google Inc. („Google“). Google Analytics verwendet sog. „Cookies“, Textdateien, die auf Ihrem Computer gespeichert werden und die eine Analyse der Benutzung der Website durch Sie ermöglichen. Die durch das Cookie erzeugten Informationen über Ihre Benutzung dieser Website werden in der Regel an einen Server von Google in den USA übertragen und dort gespeichert. Im Falle der Aktivierung der IP-Anonymisierung auf dieser Website, wird Ihre IP-Adresse von Google jedoch innerhalb von Mitgliedstaaten der Europäischen Union oder in anderen Vertragsstaaten des Abkommens über den Europäischen Wirtschaftsraum zuvor gekürzt. Nur in Ausnahmefällen wird die volle IP-Adresse an einen Server von Google in den USA übertragen und dort gekürzt. Im Auftrag des Betreibers dieser Website wird Google diese Informationen benutzen, um Ihre Nutzung der Website auszuwerten, um Reports über die Websiteaktivitäten zusammenzustellen und um weitere mit der Websitenutzung und der Internetnutzung verbundene Dienstleistungen gegenüber dem Websitebetreiber zu erbringen. Die im Rahmen von Google Analytics von Ihrem Browser übermittelte IP-Adresse wird nicht mit anderen Daten von Google zusammengeführt. Sie können die Speicherung der Cookies durch eine entsprechende Einstellung Ihrer Browser-Software verhindern; wir weisen Sie jedoch darauf hin, dass Sie in diesem Fall gegebenenfalls nicht sämtliche Funktionen dieser Website vollumfänglich werden nutzen können. Sie können darüber hinaus die Erfassung der durch das Cookie erzeugten und auf Ihre Nutzung der Website bezogenen Daten (inkl. Ihrer IP-Adresse) an Google sowie die Verarbeitung dieser Daten durch Google verhindern, indem Sie das unter dem folgenden Link: <a href="http://tools.google.com/dlpage/gaoptout?hl=de" target="new">http://tools.google.com/dlpage/gaoptout?hl=de</a> verfügbare Browser-Plugin herunterladen und installieren. Sie können die Erfassung durch Google Analytics verhindern, indem Sie auf folgenden Link klicken. Es wird ein Opt-Out-Cookie gesetzt, das die zukünftige Erfassung Ihrer Daten beim Besuch dieser Website verhindert:
-						<a href="https://tools.google.com/dlpage/gaoptout?hl=de" target="new"> Google Analytics deaktivieren </a>. Nähere Informationen zu Nutzungsbedingungen und Datenschutz finden Sie unter <a href="http://www.google.com/analytics/terms/de.html" target="new">http://www.google.com/analytics/terms/de.html</a> bzw. unter <a href="https://www.google.de/intl/de/policies/" target="new">https://www.google.de/intl/de/policies/</a>. Wir weisen Sie darauf hin, dass auf dieser Website Google Analytics um den Code „anonymizeIp“ erweitert wurde, um eine anonymisierte Erfassung von IP-Adressen (sog. IP-Masking) zu gewährleisten.
-					</p>
-				</section>
-				<section>
-					<h1 class="h--delta">Lizenzhinweise</h1>
-
-					<span class="h--delta">Konzeption:</span>
-					<span class="t--delta-serif"> Tjane Hartenstein, Elisabeth Mandl, Atelier Disko</span><br>
-
-					<span class="h--delta">Redaktion:</span>
-					<span class="t--delta-serif"> Tjane Hartenstein</span><br>
-
-					<span class="h--delta">Inhaltlich verantwortlich:</span>
-					<span class="t--delta-serif"> Abraham Taherivand</span><br>
-
-					<span class="h--delta">Design:</span>
-					<span class="t--delta-serif">Atelier Disko, <a href="https://atelierdisko.de" target="new">atelierdisko.de</a><br><br>
-
-					<p class="t--delta-serif">
-						Soweit nicht anders angegeben, stehen die Texte, Bilder, das Layout und der Code dieser Webseite unter der <a href="https://creativecommons.org/licenses/by/4.0/" target="new">Creative Commons Attribution-Lizenz (CC BY-SA) in der Version 4.0</a>.
-					</p>
-					<a href="https://creativecommons.org/licenses/by/4.0/" target="new" class="imprint__cc-logo">
-						<img src="/assets/ico/by.svg" alt="Creative Commons Logo" width="120" height="42" />
-					</a>
+						Auf <a href="https://wikimedia.de/de/impressum" target="new">https://wikimedia.de/de/impressum</a> finden Sie alle Hinweise darüber, was mit Ihren personenbezogenen Daten passiert, wenn Sie diese oder andere Websites von Wikimedia Deutschland besuchen.
 				</section>
 			</article>
 		</div>
