0e831ddd95b6c610dcb16f6ff784766891bbdc1d
Marius Wilms <mariuswilms@mailbox.org>
2018-12-17 09:09:32 +0100

    Fix update assets

---
+++ assets/js/views/layouts/default.js
@@ -0,0 +1,17 @@
+/*!
+ * Copyright 2013 David Persson. All rights reserved.
+ * Copyright 2016 Atelier Disko. All rights reserved.
+ *
+ * Use of this source code is governed by the AD General Software
+ * License v1 that can be found under https://atelierdisko.de/licenses
+ *
+ * This software is proprietary and confidential. Redistribution
+ * not permitted. Unless required by applicable law or agreed to
+ * in writing, software distributed on an "AS IS" BASIS, WITHOUT-
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+require(['domready!'], function() {
+  'use strict';
+  // ...
+});
